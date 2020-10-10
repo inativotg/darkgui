b7f15d33d561b05ca869a9b801e4bf7be583ceb0
manuzinha102 <62684382+manuzinha102@users.noreply.github.com>
2020-10-10 16:03:32 -0300

    Update DarkGUI2020.lua

--- DarkGUI2020.lua
+++ DarkGUI2020.lua
@@ -4,4 +4,4 @@
 
 print("Beta Ended;")
 print("Discord: Ma#9744")
-game.Players.LocalPlayer:Kick("Beta Ended, Discord: Ma#9744 to More.")
+game.Players.LocalPlayer:Kick("Beta Ended, Discord: Ma#9744 to More. Discord Invite: https://discord.gg/8PAzSp5")
