fa2bfe81e7f1b7ea5d7d8b1b2c01b7a44709633c
manuzinha102 <62684382+manuzinha102@users.noreply.github.com>
2020-11-08 10:28:09 -0300

    Delete DarkGUI2020.lua

--- DarkGUI2020.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/manuzinha102/darkgui/master/Settings.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/manuzinha102/darkgui/master/DarkGUI.lua", true))()
